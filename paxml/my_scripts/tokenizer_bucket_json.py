000a75070dad0d34ba99e23b411868eca0f0332d
lishengping2010@163.com <lishengping2010@163.com>
2023-10-19 13:28:06 +0800

    multi processed deal bucket json data

--- paxml/my_scripts/tokenizer_bucket_json.py
+++ paxml/my_scripts/tokenizer_bucket_json.py
@@ -1,51 +1,67 @@
 import time
+import datetime
 import os
+import re
 import sys
 import json
 import random
 import math
+import subprocess
+import logging
+import argparse
+from collections import defaultdict
 import multiprocessing
 from multiprocessing import set_start_method
-from collections import defaultdict
-import subprocess
+import socket
 
 os.environ["JAX_PLATFORMS"] = "cpu"
 
-import smart_open
 import tensorflow as tf
-import wandb
-from google.cloud import storage
 from transformers import AutoTokenizer
 
+try:
+    import smart_open
+    from google.cloud import storage
+except:
+    command = 'pip install google-cloud-storage && pip install smart_open[gcs]'
+    subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+    import smart_open
+    from google.cloud import storage
+
+# def check_bucket_exist(filepath):
+#     # filepath = 'gs://common_datasets_us-central2/pile_seq1024_tokenized/OpenSubtitles/data-00000-of-00096.test.tfrecord'
+#     command = f"gsutil stat {filepath}"
+#     response = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
+#     if response.returncode == 0:
+#         return True
+#     else:
+#         return False
 
 
+logging.basicConfig(filename='log.txt', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('Data_processed')
 
-def check_bucket_exist(filepath):
-    # filepath = 'gs://common_datasets_us-central2/pile_seq1024_tokenized/OpenSubtitles/data-00000-of-00096.test.tfrecord'
-    command = f'gsutil stat {filepath}'
-    response = subprocess.run(command, stdout=subprocess.PIPE, shell=True)
-    if response.returncode == 0:
-        return True
-    else:
-        return False
 
 class DataProcessor:
     def __init__(
         self,
         read_bucket,
-        dataset_name,
+        read_data_dir,
         tokenizer_path,
         save_dir,
         max_seq_len=2048,
         shuffle=True,
-        debug=False,
+        seed=42,
     ):
         bucket = True
         self.shuffle = shuffle
-        print("Init tokenizer.....")
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_path, revision="step3000", cache_dir="./pythia-70m-deduped/step3000",)
-
-        print("Init tokenizer finished.....")
+        logger.info("Init tokenizer.....")
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            tokenizer_path,
+            revision="step3000",
+            cache_dir="./pythia-70m-deduped/step3000",
+        )
+        logger.info("Init tokenizer finished.....")
         self.max_seq_len = max_seq_len
         self.save_dir = save_dir
         self.book_index = 0
@@ -53,14 +69,13 @@ class DataProcessor:
         self.write_line = 0
         self.writer = None
         self.clear_threshold_length = 200
-        self.split_map_dir = {'0': '_start', '1': '_median', '2': '_end'}
         self.file_count = 0
         self.book_input_ids = []
         self.read_bucket = read_bucket
-        self.dataset_name = dataset_name
+        self.read_data_dir = read_data_dir
         self.bos = 1
         self.eos = 0
-        self.debug = debug
+        self.seed = seed
         self.extract_target_filepath()
         self.extract_filepath()
 
@@ -68,51 +83,56 @@ class DataProcessor:
         return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
 
     def extract_filepath(self):
+        # 一定要设定seed，不然不同进程之间的文件顺序可能不一样
+        random.seed(self.seed)
         dataset = defaultdict(list)
         client = storage.Client()
-        for blob in client.list_blobs(self.read_bucket, prefix=self.dataset_name):
+        logger.info(f'read_bucket: {self.read_bucket}, read_data_dir: {self.read_data_dir}')
+        for blob in client.list_blobs(self.read_bucket, prefix=self.read_data_dir):
             path = os.path.join(f"gs://{self.read_bucket}", blob.name)
             if not blob.name:
                 continue
+            logger.info(f"Source path: {path}")
             if path in self.target_file_pathlist:
                 continue
-            if 'test' in blob.name:
-                dataset['test'].append(path)
+            if "test" in blob.name:
+                dataset["test"].append(path)
             else:
-                dataset['train'].append(path)
-            print(f'filename: {blob.name}===== total train: {len(dataset["train"])}; test: {len(dataset["test"])}')
+                dataset["train"].append(path)
+            logger.info(f'Filter path: {blob.name}===== total train: {len(dataset["train"])}; test: {len(dataset["test"])}')
 
         self.file_pathlist = dataset["test"] + dataset["train"]
         if self.shuffle:
             random.shuffle(self.file_pathlist)
-        if self.debug:
-            self.file_pathlist = self.file_pathlist[:10]
-        print(f'Final data: {len(self.file_pathlist)}')
+        logger.info(f"Final data: {len(self.file_pathlist)}")
 
     def extract_target_filepath(self):
         dataset = defaultdict(list)
         client = storage.Client()
         for blob in client.list_blobs(self.read_bucket, prefix=self.save_dir):
             path = os.path.join(f"gs://{self.read_bucket}", blob.name)
-            path = path.replace(self.save_dir, self.dataset_name).replace('tfrecord', 'jsonl')
+            path = path.replace(self.save_dir, self.read_data_dir)
+            re.subn(f"tfrecordF?(\d+)?", "jsonl", path)[0]
             if not blob.name:
                 continue
-            if 'test' in blob.name:
-                dataset['test'].append(path)
+            if "test" in blob.name:
+                dataset["test"].append(path)
             else:
-                dataset['train'].append(path)
-        self.target_file_pathlist = dataset["test"] + dataset["train"]
-
+                dataset["train"].append(path)
+        self.target_file_pathlist = set(dataset["test"] + dataset["train"])
 
     def convert_line_to_ids(self, line):
         return self.tokenizer.encode(line) + [self.eos]
 
-    def write_file(self):
+    def write_file(self, save_path):
         length = len(self.book_input_ids)
         start = 0
         while start < length - 1:
-            input_ids = self.book_input_ids[start: start + self.max_seq_len - 2]
-            start += (self.max_seq_len - 2)
+            if self.write_line == 0:
+                path = f"{save_path}F{self.file_count}"
+                self.writer = tf.io.TFRecordWriter(path)
+            input_ids = self.book_input_ids[start : start + self.max_seq_len - 2]
+            start += self.max_seq_len - 2
             input_ids_length = len(input_ids)
             if input_ids_length < self.max_seq_len - 2:
                 self.book_input_ids = input_ids
@@ -122,27 +142,28 @@ class DataProcessor:
                 feature = {"input_ids": self._int64_feature(input_ids)}
                 example = tf.train.Example(features=tf.train.Features(feature=feature))
                 self.writer.write(example.SerializeToString())
+                self.write_line += 1
+                if self.write_line == self.line_num_perfile:
+                    self.file_count += 1
+                    self.writer.close()
+                    self.write_line = 0
                 flag = 0
         length = len(self.book_input_ids)
         if length < self.clear_threshold_length or not flag:
             self.book_input_ids = []
-        assert len(self.book_input_ids) < self.max_seq_len, print(f'length: {len(self.book_input_ids)}')
-
+        assert len(self.book_input_ids) < self.max_seq_len, logger.info(f"length: {len(self.book_input_ids)}")
 
     def process_book(self, path):
-        # name = os.path.basename(path).rsplit('.')[0] + '.tf'
-        save_path = path.replace(self.dataset_name, self.save_dir).replace('jsonl', 'tfrecord')
-        print(f'Processed file save path: {save_path}')
-        self.writer = tf.io.TFRecordWriter(save_path)
+        save_path = path.replace(self.read_data_dir, self.save_dir).replace("jsonl", "tfrecord")
+        logger.info(f"Start to process file path: {save_path}")
         with smart_open.open(path) as fr:
             for i, line in enumerate(fr):
                 line = json.loads(line)
-                text = line['text']
+                text = line["text"]
                 ids = self.convert_line_to_ids(text)
                 self.book_input_ids.extend(ids)
                 if len(self.book_input_ids) >= self.max_seq_len - 2:
-                    self.write_file()
-        self.writer.close()
+                    self.write_file(save_path)
         self.book_index += 1
 
     def __len__(self):
@@ -154,95 +175,103 @@ class DataProcessor:
     def run(self, start, end, rank):
         N = end - start
         time_start = time.time()
-        print(f"Rank: {rank}. book: {len(self.file_pathlist)}")
+        logger.info(f"Rank: {rank}|| book: {len(self.file_pathlist)} || start: {start} || end: {end}")
         for index, path in enumerate(self.file_pathlist[start:end]):
-            print(f"Rank: {rank} index: {index}")
             try:
                 self.process_book(path)
             except Exception as e:
-                print(f'Rank: {rank}, error: {e} path: {path}')
+                logger.info(f"Rank: {rank}, error: {e} path: {path}")
             time_end = time.time()
-            print(
-                f"{rank}-processed: {index}/{N}, path: ‘{path}’ deal finished, take:"
-                f" {time_end - time_start}."
-            )
-            if rank == 0:
-                wandb_stats = {"index": index, "N": N, "take": time_end - time_start}
-                wandb.log(wandb_stats)
+            logger.info(f"{rank}-processed: {index}/{N}, path: ‘{path}’ deal finished, take: {time_end - time_start}s.")
         try:
             self.writer.close()
         except Exception as e:
-            print(f'Final close......')
+            logger.info(f"Final close......")
 
 
 def process_book_wrapper(args):
-    rank, WORKERS, max_seq_len, read_bucket, dataset_name, host_id, host_num = args
-    bucket = True
-    tokenizer_path = "/nas2/lishengping/other_models/baichuan2-13b-base"
-    if not os.path.exists(tokenizer_path):
-        tokenizer_path = "EleutherAI/pythia-70m-deduped"
-    if bucket:
-        save_dir = f'pile_seq1024_tokenized/'
-    else:
-        save_dir = f"pile_processed_seq1024/"
-    debug = False
+    rank, workers, max_seq_len, read_bucket, read_data_dir, host_id, host_num, tokenizer_path, save_dir, seed, line_num_perfile = args
     processor = DataProcessor(
-        read_bucket,
-        dataset_name,
-        tokenizer_path,
-        save_dir,
-        max_seq_len=max_seq_len,
-        debug=debug
+        read_bucket, read_data_dir, tokenizer_path, save_dir, max_seq_len=max_seq_len, seed=seed
     )
-    host_num = int(host_num)
-    host_id = int(host_id)
     processor.rank = rank
     processor.host_id = host_id
     processor.host_num = host_num
-    every_host_nums = math.ceil(len(processor.file_pathlist) / host_num)
-    processor.file_pathlist = processor.file_pathlist[host_id *  every_host_nums :(host_id + 1) * every_host_nums]
+    processor.line_num_perfile = line_num_perfile
 
-    every_rank_nums = math.ceil(len(processor.file_pathlist) / WORKERS)
-    if rank == 0:
-        wandb.login(key="7988c805dfe3fed4d6e4017f616555a5160fd2c2")
-        wandb.init(project="pile_test", name="data_processed", config=None, resume=True)
+    every_host_nums = math.ceil(len(processor.file_pathlist) / host_num)
+    processor.file_pathlist = processor.file_pathlist[host_id * every_host_nums : (host_id + 1) * every_host_nums]
+    every_rank_nums = math.ceil(len(processor.file_pathlist) / workers)
     start = int(rank * every_rank_nums)
     end = int((rank + 1) * every_rank_nums)
-    print(f"Rank: {rank} start: {start} end: {end}")
     processor.run(start, end, rank)
     return rank
 
 
+def split_bucket_and_dir(path):
+    if "gs://" in path:
+        subdir = path[5:]
+    bucket, subdir = subdir.split("/", maxsplit=1)
+    return bucket, subdir + "/" if not subdir.endswith('/') else subdir
+
+
 if __name__ == "__main__":
-    random.seed(42)
-    host_num = sys.argv[1]
-    host_id = sys.argv[2]
+    parser = argparse.ArgumentParser(description="Data process script")
+    parser.add_argument("--read_dir", type=str, help="Data read cloud bucket dir.")
+    parser.add_argument("--save_dir", type=str, help="Save model weight file path, it is a dir.")
+    parser.add_argument("--host_num", type=int, default=1, help="vm host numbers")
+    parser.add_argument("--seed", type=int, default=42, help="Program shuffle seed")
+    parser.add_argument("--tokenizer_path", type=str, help="Tokenizer path")
+    parser.add_argument("--max_seq_len", type=int, default=1024, help="Tokenizer max sequence length")
+    parser.add_argument("--workers", type=int, default=10, help="Processed program numbers at same time")
+    parser.add_argument("--line_num_perfile", type=int, default=10000, help="Save data numbers per file when length is max_seq_len")
+
+
+    hostname = socket.gethostname()
+    args = parser.parse_args()
+
+    seed = args.seed
+    # data_full_dir = 'gs://common_datasets_us-central2/pile/'
+    data_full_dir = args.read_dir
+    # save_dir = f"pile_seq2048_tokenized/"
+    save_dir = args.save_dir
+    # tokenizer_path = "EleutherAI/pythia-70m-deduped"
+    tokenizer_path = args.tokenizer_path
+    host_num = args.host_num
+    host_id = hostname
+    # 基于tpu vm $hostname环境变量传递，但是需要进行处理，得到编号
+    if isinstance(host_id, str) and len(host_id) > 5:
+        host_id = host_id.rsplit("-", maxsplit=1)[-1]
+    host_id = int(host_id)
+    host_num = int(host_num)
+
+    assert host_id < host_num
+    read_bucket, read_data_dir = split_bucket_and_dir(data_full_dir)
+    assert read_data_dir[0] != "/" and read_data_dir[-1] == "/"
+
+    max_seq_len = args.max_seq_len
+    line_num_perfile = args.line_num_perfile
+    workers = args.workers
 
     set_start_method("spawn")  # tpu-vm
 
-    read_bucket = 'common_datasets_us-central2'
-    # 文件夹的prefix
-    dataset_name = 'pile/'
+    random.seed(seed)
     num_processes = multiprocessing.cpu_count()
-    print(f"num_processes: {num_processes}")
-    WORKERS = 200
-    max_seq_len = 1024
-    # host_id = 0
-    pool = multiprocessing.Pool(processes=WORKERS)
+    logger.info(f"num_processes: {num_processes}")
+    pool = multiprocessing.Pool(processes=workers)
     args = (
-        [rank, WORKERS, max_seq_len, read_bucket, dataset_name, host_id, host_num]
-        for rank in range(WORKERS)
+        [rank, workers, max_seq_len, read_bucket, read_data_dir, host_id, host_num, tokenizer_path, save_dir, seed, line_num_perfile]
+        for rank in range(workers)
     )
+    logger.info(f'args: {args}')
+
     results = pool.map(process_book_wrapper, args)  # 包含每个进程的返回值
     pool.close()
     pool.join()
 
 # https://huggingface.co/datasets/ArmelR/the-pile-splitted
-# Usage: using multi-host and multi-processe to deal data.
-# python processed.py 4 0/1/2/3
-# packages:
-# pip install google-cloud-storage
-# pip install smart_open[gcs]
-# pip install wandb
-# wandb disabled
-# vi processed.py
+
+# gcloud compute tpus tpu-vm scp paxml/my_scripts/tokenizer_bucket_json.py llm-jax-v4-64-3:~/  --zone=us-central2-b  --worker=all  --project=llm-tpu
+# gcloud compute tpus tpu-vm ssh llm-jax-v4-64-3 --zone=us-central2-b --worker=all --command="pkill -f 'python tokenizer_bucket_json.py'; /home/lishengping/miniconda3/bin/python tokenizer_bucket_json.py --read_dir gs://common_datasets_us-central2/pile/ --save_dir pile_seq4096_tokenized/ --tokenizer_path EleutherAI/pythia-70m-deduped --max_seq_len 4096 --host_num 8 --workers 4"
+# gcloud compute tpus tpu-vm ssh llm-jax-v4-64-3 --zone=us-central2-b --worker=all --command="pkill -f 'python tokenizer_bucket_json.py'"
+# python processed.py --read_dir gs://common_datasets_us-central2/pile/  --save_dir pile_seq4096_tokenized/ --host_num 4  --tokenizer_path EleutherAI/pythia-70m-deduped --max_seq_len 4096 --workers 4

177596504fed5e667e988bc3e7fd4f63a4e75033
lishengping2010@163.com <lishengping2010@163.com>
2024-04-19 13:21:46 +0800

    v5p auto train

--- paxml/my_scripts/v5p_auto_train.sh
+++ paxml/my_scripts/v5p_auto_train.sh
@@ -1,7 +1,7 @@
-TPU_TYPE=v5p-16
+TPU_TYPE=v5p-256
 ZONE=us-east5-a
 CREATE_ARGS="--runtime-version v2-alpha-tpuv5"
-TPU_NAME=llm-jax-$TPU_TYPE-11
+TPU_NAME=llm-jax-$TPU_TYPE-10
 PROJECT_ID="ntpu-413714"
 EXP="PileDCSlimLlama7B4Kx4x256x1v5p"
 INSTALL=$1
@@ -15,33 +15,41 @@ echo EXP is $EXP
 echo INSTALL is $INSTALL
 echo TRAIN is $TRAIN
 
+
+FLAG=0
+
 while true
 do
     tpu_status=$(gcloud alpha compute tpus describe $TPU_NAME --zone=$ZONE --project $PROJECT_ID --format="value[terminator=''](state)")
     echo Tpu status is ${tpu_status}
 
-    if [ -z "$tpu_status" ] || [ "$tpu_status" != "READY" ] && [ "$tpu_status" != "CREATING" ]; then
-        echo 'TPU is not existed, now start to create......'
-        gcloud compute tpus tpu-vm delete ${TPU_NAME} --zone=${ZONE} --project $PROJECT_ID --quiet
-        sleep 30s
-        echo 'Y' | gcloud alpha compute tpus queued-resources delete $TPU_NAME --zone=$ZONE  --project $PROJECT_ID
-        sleep 10s
-        gcloud alpha compute tpus queued-resources create $TPU_NAME --node-id $TPU_NAME  --project $PROJECT_ID   --zone=$ZONE   --accelerator-type=$TPU_TYPE ${CREATE_ARGS} --service-account 887571727717-compute@developer.gserviceaccount.com   --best-effort
-        sleep 1m
-
-    elif [ $tpu_status == "CREATING" ]; then
-        echo 'TPU is creating......'
-        sleep 30s
-        continue
-
-    else
+    if [ "$tpu_status" == "READY" ];then
+        FLAG=0
         echo 'Start training......'
-        if INSTALL; then
+        if $INSTALL;then
             gcloud compute tpus tpu-vm scp /home/lishengping/lsp/install_0812.sh  ${TPU_NAME}:~/  --zone=$ZONE  --project $PROJECT_ID --worker=all
             gcloud compute tpus tpu-vm ssh ${TPU_NAME} --zone=$ZONE --project $PROJECT_ID --worker=all --command="bash install_0812.sh ${ZONE:0:-2} 2>&1 | tee install.log"
         fi
-        if TRAIN; then
+        if $TRAIN;then
             gcloud compute tpus tpu-vm ssh $TPU_NAME --zone=$ZONE --worker=all --command="killall main.py;sudo lsof -w /dev/accel0 |cut -c 9-14|awk 'NR>1 {print $1}'| xargs sudo kill -9; sudo rm -f /tmp/libtpu_lockfile;sudo chmod +777 -R /tmp/tpu_logs/; /home/lishengping/miniconda3/bin/python /home/lishengping/projects/paxml/paxml/main.py --exp=tasks.lm.params.c4.$EXP --job_log_dir=gs://llm_base_models_us-east5/v5p_256/7B/$EXP 2>&1 --enable_checkpoint_saving=False --eval_on_test=False | tee train.log" --project=$PROJECT_ID
         fi
+    elif [ "$tpu_status" == "CREATING" ];then
+        FLAG=1
+        echo 'TPU is creating......'
+        sleep 30s
+    elif [ $FLAG == 0 ];then
+        echo 'TPU is not existed, now start to create......'
+        gcloud compute tpus tpu-vm delete ${TPU_NAME} --zone=${ZONE} --project $PROJECT_ID --quiet
+        echo 'Y' | gcloud alpha compute tpus queued-resources delete $TPU_NAME --zone=$ZONE  --project $PROJECT_ID
+        sleep 15s
+        gcloud alpha compute tpus queued-resources create $TPU_NAME --node-id $TPU_NAME  --project $PROJECT_ID   --zone=$ZONE   --accelerator-type=$TPU_TYPE ${CREATE_ARGS} --service-account 887571727717-compute@developer.gserviceaccount.com   --best-effort
+        sleep 1m
+        FLAG=1
+    else
+        FLAG=1
+        echo 'TPU is creating......'
+        sleep 30s
+
+    # if [ -z "$tpu_status" ] || [ "$tpu_status" != "READY" ] && [ "$tpu_status" != "CREATING" ]; then
     fi
 done
